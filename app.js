bf8392d3bcb70c186081be0beb1542f71ccdf4fb
OKitel <kitelolga@gmail.com>
2021-03-26 03:19:52 +0100

    fix: fix edit button

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ var editTask=function(){
 
   var listItem=this.parentNode;
 
-  var editInput=listItem.querySelector(".input-text");
+  var editInput=listItem.querySelector('input[type=text]');
   var label=listItem.querySelector("label");
   var editBtn=listItem.querySelector(".edit-button");
   var containsClass=listItem.classList.contains("edit-mode");
